ef1dca62259500627024aefc269183c6b7929253
Jaiveer Katariya <jaiveerkatariya@Jaiveers-MBP.lan>
2022-07-21 12:00:15 -0400

    second try

--- handler.go
+++ handler.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"github.com/golang/freetype"
@@ -47,6 +48,7 @@ type WyrResponse struct {
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
+	log.Print("received request...")
 	blankResponse := &WyrResponse{}
 	grabContent(blankResponse)
 	log.Printf("got response from grabContent: %s", blankResponse.Data)
@@ -54,15 +56,15 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("converting to PNG...")
 
 	//imgBytes, err := contentToImage(blankResponse.Data)
-	img := contentToImage(blankResponse.Data)
-	err := png.Encode(w, img)
+	imgBytes, err := contentToImage(blankResponse.Data)
+	//err := png.Encode(w, img)
 	if err != nil {
 		log.Printf("ERROR: while encoding image to PNG: %s", err)
 	}
 
 	w.WriteHeader(200)
-	w.Header().Set("Content-Type", "application/octet-stream")
-	//w.Write(imgBytes)
+	w.Header().Set("Content-Type", "image/png")
+	w.Write(imgBytes)
 	return
 }
 
@@ -88,7 +90,7 @@ func grabContent(target *WyrResponse) {
 	}
 }
 
-func contentToImage(content string) image.Image {
+func contentToImage(content string) ([]byte, error) {
 	img := image.NewRGBA(image.Rect(0, 0, 9*len(content), 50))
 	col := color.RGBA{R: 0, G: 0, B: 0, A: 255}
 	point := fixed.Point26_6{X: fixed.I(20), Y: fixed.I(30)}
@@ -101,48 +103,12 @@ func contentToImage(content string) image.Image {
 	}
 	d.DrawString(content)
 
-	return img
-	//text := strings.Split(content, "\n")
-	//
-	//var fontSize float64 = 12
-	//
-	//fgColor := color.RGBA{R: 0xff, G: 0xff, B: 0xff, A: 0xff}
-	//bgColor := color.RGBA{R: 0x00, G: 0x00, B: 0x00, A: 0xff}
-	//
-	//fg := image.NewUniform(fgColor)
-	//bg := image.NewUniform(bgColor)
-	//
-	//imgLength := 7 * len(content)
-	//
-	//rgba := image.NewRGBA(image.Rect(0, 0, imgLength, 50))
-	//draw.Draw(rgba, rgba.Bounds(), bg, image.Pt(0, 0), draw.Src)
-	//c := freetype.NewContext()
-	//c.SetDPI(72)
-	//c.SetFont(f)
-	//c.SetFontSize(fontSize)
-	//c.SetClip(rgba.Bounds())
-	//c.SetDst(rgba)
-	//c.SetSrc(fg)
-	//c.SetHinting(font.HintingNone)
-	//
-	//textXOffset := len(content) / 2
-	//textYOffset := 10 + int(c.PointToFixed(fontSize)>>6) // Note shift/truncate 6 bits first
-	//
-	//pt := freetype.Pt(textXOffset, textYOffset)
-	//for _, s := range text {
-	//	_, err := c.DrawString(strings.Replace(s, "\r", "", -1), pt)
-	//	if err != nil {
-	//		return nil, err
-	//	}
-	//	pt.Y += c.PointToFixed(fontSize * 1.5)
-	//}
-	//
-	//b := new(bytes.Buffer)
-	//if err := png.Encode(b, rgba); err != nil {
-	//	log.Printf("ERROR: unable to encode image: %s", err)
-	//	return nil, err
-	//}
-	//return b.Bytes(), nil
+	b := new(bytes.Buffer)
+	if err := png.Encode(b, img); err != nil {
+		log.Println("unable to encode image.")
+		return nil, err
+	}
+	return b.Bytes(), nil
 }
 
 func main() {
